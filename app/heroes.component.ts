e006b1e3c153bec6cda3f011bab1e299ab389457
raimu <ortwang@googlemail.com>
2016-11-27 20:40:45 +0100

    End of Chapter 7: HTTP
    Now after the seventh chapter of the tutorial, we can now create
    update, delete and search heroes over the fake rest api.

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -26,6 +26,25 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
@@ -33,4 +52,4 @@ export class HeroesComponent implements OnInit {
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
- }
+}
